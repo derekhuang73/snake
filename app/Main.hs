db1746d689282e0ed408ca2a19f3e95047ef744c
bradyluo <chendong.luo@icloud.com>
2024-02-24 17:26:52 -0800

    add score panel and redefine boundaries

--- app/Main.hs
+++ app/Main.hs
@@ -7,7 +7,7 @@ windowWidth, windowHeight, cellSize :: Int
 windowWidth = 800
 windowHeight = 600
 cellSize = 20
-dietClock = 5
+dietClock = 10
 widthCells = 40
 heightCells = 30
 
@@ -18,7 +18,8 @@ data GameState = GameState {
     snake :: [(Int, Int)],
     food :: (Int, Int),
     direction :: Direction,
-    gameOver :: Bool
+    gameOver :: Bool,
+    score :: Int
 }
 
 type Snake = [(Int, Int)] 
@@ -26,22 +27,31 @@ type Food = (Int, Int)
 initialState :: GameState
 initialState = GameState {
     gameTime = 0,
-    snake = [(0, 0)],
+    snake = [(10, 10)],
     food = (20, 10),
     direction = Main.Right,
-    gameOver = False
+    gameOver = False,
+    score = 0
 }
 
 drawGameState :: GameState -> Picture
-drawGameState gs = pictures [boundary, snakePic, foodPic, gameOverPic]
+drawGameState gs = pictures [boundary, snakePic, foodPic, gameOverPic, scorePic]
     where
-        snakePic = color blue $ pictures $ map drawCell (snake gs)
+        snakePic = color blue $ pictures $ map drawSnake (snake gs)
         foodPic = color red $ drawCell (food gs)
         drawCell (x, y) = translate (fromIntegral $ x * cellSize - fromIntegral windowWidth `div` 2 + fromIntegral cellSize `div` 2) 
                           (fromIntegral $ y * cellSize - fromIntegral windowHeight `div` 2 + fromIntegral cellSize `div` 2) 
-                          $ rectangleSolid (fromIntegral cellSize) (fromIntegral cellSize)
-        gameOverPic = if gameOver gs then translate (-200) 0 $ scale 0.3 0.3 $ color (dark red) $ text "Game Over" else blank
-        boundary = color black $ rectangleWire (fromIntegral windowWidth) (fromIntegral windowHeight)
+                          $ circleSolid (fromIntegral cellSize / 2)
+        drawSnake (x, y) = translate (fromIntegral $ x * cellSize - fromIntegral windowWidth `div` 2 + fromIntegral cellSize `div` 2) 
+                          (fromIntegral $ y * cellSize - fromIntegral windowHeight `div` 2 + fromIntegral cellSize `div` 2) 
+                          $ pictures [color (white) $ rectangleWire (fromIntegral cellSize) (fromIntegral cellSize), 
+                          color (blue) $ rectangleSolid (fromIntegral cellSize - 1) (fromIntegral cellSize - 1)]
+        gameOverPic = if gameOver gs then pictures [translate (-200) 0 $ scale 0.5 0.5 $ color (dark red) $ text "Game Over", translate (-150) (-50) $ scale 0.2 0.2 $ color (dark red) $ text "Press R to Restart"] else blank
+        boundary = color black $ rectangleWire (fromIntegral $ windowWidth - 4 * cellSize) (fromIntegral $ windowHeight - 4 * cellSize)
+        scorePic = translate (-fromIntegral windowWidth / 2 + 20) (fromIntegral windowHeight / 2 - 20) 
+                   $ scale 0.2 0.2 
+                   $ color black 
+                   $ text ("Score: " ++ show (score gs))
 
 handleInput :: Event -> GameState -> GameState
 handleInput (EventKey (Char 'r') Graphics.Gloss.Interface.Pure.Game.Down _ _) gs = initialState
@@ -54,9 +64,9 @@ handleInput _ gs = gs
 updateGameState :: Float -> GameState -> GameState
 updateGameState time gs
     | gameOver gs = gs
-    | otherwise = if collidedWithWall || collidedWithSnake || starve then gs { gameOver = True } else gs { snake = newDietSnake, gameTime = newDietGameTime, food = newFood}
+    | otherwise = if collidedWithWall || collidedWithSnake || starve then gs { gameOver = True } else gs { snake = newDietSnake, gameTime = newDietGameTime, food = newFood, score = newScore}
     where
-        collidedWithWall = x < 0 || x >= windowWidth `div` cellSize || y < 0 || y >= windowHeight `div` cellSize
+        collidedWithWall = x < 2 || x >= (windowWidth) `div` cellSize - 2|| y < 2 || y >= (windowHeight) `div` cellSize - 2
         collidedWithSnake = (x, y) `elem` tail (snake gs)
         didEatFlag = didEat (snake gs) (direction gs) (food gs)
         newSnake = moveSnake (snake gs) (direction gs) (food gs)
@@ -66,6 +76,7 @@ updateGameState time gs
         newFood = updateFood (food gs) didEatFlag
         starve = (snake gs == [])
         (x, y) = head newDietSnake
+        newScore = if didEatFlag then score gs + 1 else score gs
 
 moveSnakehead :: (Int, Int) -> Direction -> (Int, Int) 
 moveSnakehead (x,y) dir = 
